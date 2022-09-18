b577a220751949f9edece4304c748bf5a55c7356
Koshal Parwan <parwan.kp@pg.com>
2022-09-18 19:25:58 +0400

    [feat] contact me

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
 import { motion } from 'framer-motion'
+import Link from 'next/link';
 
 type Props = {}
 
@@ -44,33 +45,35 @@ function Header({ }: Props) {
                 />
             </motion.div>
 
-            <motion.div
-                initial={{
-                    x: 500,
-                    opacity: 0,
-                    scale: 0.5
-                }}
-                animate={{
-                    x: 0,
-                    opacity: 1,
-                    scale: 0.5
-                }}
-                transition={{
-                    duration: 1.5,
-                }}
-                className='flex flex-row items-center text-gray-300 cursor-pointer'
-            >
-                <SocialIcon
-                    className='cursor-pointer'
-                    network='email'
-                    fgColor='gray'
-                    bgColor='transparent'
-                />
+            <Link href="#contactme">
+                <motion.div
+                    initial={{
+                        x: 500,
+                        opacity: 0,
+                        scale: 0.5
+                    }}
+                    animate={{
+                        x: 0,
+                        opacity: 1,
+                        scale: 0.5
+                    }}
+                    transition={{
+                        duration: 1.5,
+                    }}
+                    className='flex flex-row items-center text-gray-300 cursor-pointer'
+                >
+                    <SocialIcon
+                        className='cursor-pointer'
+                        network='email'
+                        fgColor='gray'
+                        bgColor='transparent'
+                    />
 
-                <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
-                    Get In Touch
-                </p>
-            </motion.div>
+                    <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
+                        Get In Touch
+                    </p>
+                </motion.div>
+            </Link>
 
         </header>
     )
